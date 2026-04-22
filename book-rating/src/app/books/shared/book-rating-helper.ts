6f949d93b088f4522af06a248bf436ad9f7c786f
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2026-04-22 15:42:15 +0200

    BookRatingHelper test

--- book-rating/src/app/books/shared/book-rating-helper.ts
+++ book-rating/src/app/books/shared/book-rating-helper.ts
@@ -7,12 +7,20 @@ import { Book } from './book';
 })
 export class BookRatingHelper {
 
+  readonly minRating = 1;
+  readonly maxRating = 5;
+
   rateUp(book: Book): Book {
-    return book; // TODO
+    return {
+      ...book,
+      rating: Math.min(book.rating + 1, this.maxRating)
+    }
   }
 
   rateDown(book: Book): Book {
-    return book; // TODO
+    return {
+      ...book,
+      rating: Math.max(book.rating - 1, this.minRating)
+    }
   }
-
 }
