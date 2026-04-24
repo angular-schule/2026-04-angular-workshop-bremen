31cf0846a882fa0a1b72dc8010ff0a5d82afd6ab
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2026-04-24 19:11:24 +0200

    book-rating: final rxjs example

--- book-rating/src/app/books/book-details-page/book-details-page.ts
+++ book-rating/src/app/books/book-details-page/book-details-page.ts
@@ -1,11 +1,11 @@
-import { httpResource } from '@angular/common/http';
+import { HttpErrorResponse, httpResource } from '@angular/common/http';
 import { Component, inject, input } from '@angular/core';
 import { Book } from '../shared/book';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { BookStore } from '../shared/book-store';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { JsonPipe } from '@angular/common';
-import { map, mergeMap } from 'rxjs';
+import { catchError, concatMap, map, mergeMap, of, retry, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-book-details-page',
@@ -15,19 +15,27 @@ import { map, mergeMap } from 'rxjs';
 })
 export class BookDetailsPage {
 
+  // Buch laden mit RxJS
+
   router = inject(ActivatedRoute);
   bookStore = inject(BookStore);
 
   book2 = toSignal(this.router.paramMap.pipe(
     map(paramMap => paramMap.get('isbn') || ''),
-    mergeMap(isbn => this.bookStore.getSingle(isbn))
+    switchMap(isbn => this.bookStore.getSingle(isbn).pipe(
+      retry({
+        count: 2,
+        delay: 500
+      }),
+      catchError((err: HttpErrorResponse) => of({
+        isbn: '000',
+        title: 'Error',
+        description: err.message
+      }))
+    )),
   ))
 
-
-
-
-
-
+  // Buch laden mit httpResource
   readonly isbn = input.required<string>();
   readonly book = httpResource<Book>(
     () => `https://api.angular.schule/books/${this.isbn()}`
