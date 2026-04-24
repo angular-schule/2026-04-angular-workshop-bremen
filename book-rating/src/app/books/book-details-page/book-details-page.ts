a9c10463612e01b70a13725bf03b9d5e31c35a3f
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2026-04-24 11:04:04 +0200

    routing + httpResource

--- book-rating/src/app/books/book-details-page/book-details-page.ts
+++ book-rating/src/app/books/book-details-page/book-details-page.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { httpResource } from '@angular/common/http';
+import { Component, input } from '@angular/core';
+import { Book } from '../shared/book';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-book-details-page',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './book-details-page.html',
   styleUrl: './book-details-page.scss',
 })
-export class BookDetailsPage {}
+export class BookDetailsPage {
+
+  readonly isbn = input.required<string>();
+  readonly book = httpResource<Book>(
+    () => `https://api.angular.schule/books/${this.isbn()}`
+  )
+}
