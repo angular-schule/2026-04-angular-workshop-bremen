5f7f82da7380706939471f629912bf9b5c446e31
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2026-04-24 14:28:35 +0200

    rxjs: game score

--- rxjs-playground/src/app/exercise-gamescore/exercise-gamescore.ts
+++ rxjs-playground/src/app/exercise-gamescore/exercise-gamescore.ts
@@ -23,7 +23,14 @@ export class ExerciseGamescore {
 
     /******************************/
 
-    
+    this.score$.pipe(
+      scan((acc, item) => acc + item, 0)
+    ).subscribe(score => this.currentScore.set(score));
+
+    this.score$.pipe(
+      reduce((acc, item) => acc + item, 0)
+    ).subscribe(score => this.finalScore.set(score));
+
     /******************************/
 
     this.score$.subscribe({
